f7b39d8d6da251d3aa3aedee90516b07d0c9ce02
Denis Buryachkovskiy <denis.buryachkovskiy@dataart.com>
2019-01-10 15:55:07 +0200

    Removed credentials file

--- src/firebase.credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseCredentials = {
-    apiKey: "AIzaSyAW-ngEzhoPlOj2eSt3gjs4zOH0ToA0VuA",
-    authDomain: "progressive-study.firebaseapp.com",
-    databaseURL: "https://progressive-study.firebaseio.com",
-    projectId: "progressive-study",
-    storageBucket: "progressive-study.appspot.com",
-    messagingSenderId: "517831378717"
-}
